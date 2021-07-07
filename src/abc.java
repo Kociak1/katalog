c8e2dad3e3b9fd0073fd1fe444e3855ed43fb680
Kociak1 <patr-01@wp.pl>
2021-07-07 21:16:58 +0200

    teraz działa

--- src/abc.java
+++ src/abc.java
@@ -1,6 +1,6 @@
 public class abc {
     public static void main(String[] args) {
-        System.out.print("Hellow 2");
+        System.out.print("Hellow 77");
     }
     private void displayText() {
         System.out.print("witaj tu");
