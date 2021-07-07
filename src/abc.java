b1e8a1d1b8ee51bb99701c2a5de8c33c4dd45b48
Kociak1 <patr-01@wp.pl>
2021-07-07 21:11:11 +0200

    wynik

--- src/abc.java
+++ src/abc.java
@@ -2,4 +2,7 @@ public class abc {
     public static void main(String[] args) {
         System.out.print("Hellow World");
     }
+    private void displayText() {
+        System.out.print("witaj tu");
+    }
 }
